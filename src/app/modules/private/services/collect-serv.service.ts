c3de6801c857a501e1cb081077f05b1fd6a13066
matheusgevartosky <magevartosky@gmail.com>
2022-05-24 13:07:47 -0300

    Finaliza Layout e Forms

--- src/app/modules/private/services/collect-serv.service.ts
+++ src/app/modules/private/services/collect-serv.service.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class CollectServService {
 
-  constructor() { }
+  constructor(private _http: HttpClientModule) { }
+
+
+  
+
 }
